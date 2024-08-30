75d80ef659c9a4002cda511870c64887057f04d0
Shawn Kebel <fxrender@outlook.com>
2024-08-29 22:03:27 -0500

    :sparkles::safety_vest: Create `email verification template` for `user registration`

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -10,3 +10,28 @@ const transporter = nodemailer.createTransport({
     pass: process.env.ZOHO_USER_PASS,
   },
 });
+  //   mail options
+  const mailOptions = {
+    from: ZOHO_AUTH_USER,
+    to: email,
+    subject: "Email Verification",
+    html: `<div
+    style='font-family: Arial, sans-serif; font-size: 20px; color: #333; background-color: #f7f7f7; padding: 20px; border-radius: 5px;'>
+    <h3 style="color: rgb(8, 56, 188)">Please verify your email address</h3>
+    <hr>
+    <h4>Hi ${firstName},</h4>
+    <p>
+        Please verify your email address so we know it's really you.
+        <br>
+    <p>This link <b>expires in 1 hour</b></p>
+    <br>
+    <a href=${link}
+        style="color: #fff; padding: 14px; text-decoration: none; background-color: #000;  border-radius: 8px; font-size: 18px;">Verify
+        Email Address</a>
+    </p>
+    <div style="margin-top: 20px;">
+        <h5>Best Regards</h5>
+        <h5>S͛OͦCͨIͥAͣLiͥzeͤ Team</h5>
+    </div>
+</div>`,
+  };
