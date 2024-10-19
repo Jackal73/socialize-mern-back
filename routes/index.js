546cf13f09f13635052005e73bcc9d6e954cedfd
Shawn Kebel <fxrender@outlook.com>
2024-10-19 10:56:18 -0500

    :necktie: Add `user routes`

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import authRoute from "./authRoutes.js";
+import userRoute from "./userRoutes.js";
 const router = express.Router();
 router.use(`/auth`, authRoute); //auth/register
+router.use(`/users`, userRoute);
 export default router;
